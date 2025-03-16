760ac42e87dabcdb045374750589cecfc88be2b2
NandhipatiGanesh <162296386+ganeshadvait@users.noreply.github.com>
2025-03-16 22:43:49 +0530

    s

--- app/components/mobilsticky.jsx
+++ app/components/mobilsticky.jsx
@@ -7,7 +7,9 @@ const MobileStickyFooter = () => {
       <div className="w-full flex justify-between items-stretch">
         {/* WhatsApp CTA */}
         <div className="flex-1 bg-purple-600 text-center py-3 ">
-          <a href="https://wa.me/your-number" className="flex flex-col items-center text-white text-[12px] text-sm font-500">
+          <a href="https://wa.me/your-number" className="flex flex-col items-center text-white text-[12px] text-sm font-500" style={{
+            rowGap:'5px'
+          }}>
             <Image src="/whatsapp.svg" alt="WhatsApp" width={24} height={24} />
             <span> WhatsApp</span>
           </a>
@@ -15,7 +17,9 @@ const MobileStickyFooter = () => {
 
         {/* Book Appointment CTA */}
         <div className="flex-1 bg-purple-600 text-center py-3">
-          <a href="your-booking-link" className="flex flex-col items-center text-white text-[12px] font-500">
+          <a href="your-booking-link" className="flex flex-col items-center text-white text-[12px] font-500"style={{
+            rowGap:'5px'
+          }}>
             <Image src="/schedule.svg" alt="Book Appointment" width={24} height={24} />
             <span>Book An Appointment</span>
           </a>
@@ -23,7 +27,9 @@ const MobileStickyFooter = () => {
 
         {/* Online Consultation CTA */}
         <div className="flex-1 bg-purple-600 text-center py-3">
-          <a href="your-online-consultation-link" className="flex flex-col  text-white items-center text-[12px] text-sm font-500">
+          <a href="your-online-consultation-link" className="flex flex-col  text-white items-center text-[12px] text-sm font-500"style={{
+            rowGap:'5px'
+          }}>
             <Image src="/telemedicine.svg" alt="Online Consultation" width={24} height={24} />
             <span>Online <br></br> Consultation</span>
           </a>
