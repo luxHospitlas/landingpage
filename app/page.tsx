936773b8e3e323f8be077190696fc1bffbbfe29a
NandhipatiGanesh <126049941+NandhipatiGanesh@users.noreply.github.com>
2025-03-21 22:56:10 +0530

    dynamic added

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,65 @@
+import dynamic from 'next/dynamic';
 
+// Static imports (always needed immediately)
 import Hero from './components/hero/hero';
-import Faqs from './components/faqs/faq';
-import Whatare from './components/what/whatare';
-import Luxgpt from './components/luxgpt/luxgpt';
-import VideoSection from './components/Videosection/videos';
-import PatientTalks from './components/Patienttalks/patients';
 import Second from './components/second/second';
 import HospitalLocation from './components/HospitalLocation/hospitallocations';
-import HospitalComparison from './components/Treatmenttable/Hospitalcomparison';
-import Accreditations from './components/Accreditions/accreditions';
-import DoctorCard from './components/topdoctors/ourdoctors';
+
+// Dynamic imports (lazy-loaded for better performance)
+const DoctorCard = dynamic(() => import('./components/topdoctors/ourdoctors'));
+const HospitalComparison = dynamic(() => import('./components/Treatmenttable/Hospitalcomparison'));
+const Accreditations = dynamic(() => import('./components/Accreditions/accreditions'));
+const VideoSection = dynamic(() => import('./components/Videosection/videos'));
+const PatientTalks = dynamic(() => import('./components/Patienttalks/patients'));
+const Luxgpt = dynamic(() => import('./components/luxgpt/luxgpt'));
+const Whatare = dynamic(() => import('./components/what/whatare'));
+const Faqs = dynamic(() => import('./components/faqs/faq'));
+
 export default function Home() {
   return (
     <>
-    <Hero />
-    <Second />
-    <HospitalLocation />
-    <DoctorCard />
-    <HospitalComparison />
-    <Accreditations />
-    <VideoSection />
-    <PatientTalks />
-    <Luxgpt />
-    <Whatare />
-    <Faqs />
+      <Hero />
+      <Second />
+      <HospitalLocation />
+      <DoctorCard />
+      <HospitalComparison />
+      <Accreditations />
+      <VideoSection />
+      <PatientTalks />
+      <Luxgpt />
+      <Whatare />
+      <Faqs />
     </>
   );
 }
+
+
+
+// import Hero from './components/hero/hero';
+// import Faqs from './components/faqs/faq';
+// import Whatare from './components/what/whatare';
+// import Luxgpt from './components/luxgpt/luxgpt';
+// import VideoSection from './components/Videosection/videos';
+// import PatientTalks from './components/Patienttalks/patients';
+// import Second from './components/second/second';
+// import HospitalLocation from './components/HospitalLocation/hospitallocations';
+// import HospitalComparison from './components/Treatmenttable/Hospitalcomparison';
+// import Accreditations from './components/Accreditions/accreditions';
+// import DoctorCard from './components/topdoctors/ourdoctors';
+// export default function Home() {
+//   return (
+//     <>
+//     <Hero />
+//     <Second />
+//     <HospitalLocation />
+//     <DoctorCard />
+//     <HospitalComparison />
+//     <Accreditations />
+//     <VideoSection />
+//     <PatientTalks />
+//     <Luxgpt />
+//     <Whatare />
+//     <Faqs />
+//     </>
+//   );
+// }
