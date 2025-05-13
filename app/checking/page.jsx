c80192cf5e7a95268e9350d6bfaa8218b2e26652
Ganesh <205735160+luxHospitlas@users.noreply.github.com>
2025-05-13 17:47:56 +0530

    id changed

--- app/checking/page.jsx
+++ app/checking/page.jsx
@@ -20,7 +20,10 @@ export default function CheckingPage() {
   }, []);
 
   return (
-    <div className="mt-6 flex items-center justify-center md:justify-start space-x-2 bg-white rounded-full px-4 py-2 w-fit">
+    <div
+      className="mt-6 flex items-center justify-center md:justify-start space-x-2 bg-white rounded-full px-4 py-2 w-fit"
+      id="whatsappBtn"
+    >
       <input
         type="text"
         id="messageInput"
@@ -28,7 +31,7 @@ export default function CheckingPage() {
         className="bg-transparent text-gray-700 focus:outline-none"
       />
       <div>
-        <button id="whatsappBtn">➜</button>
+        <span>➜</span>
       </div>
     </div>
   );
