a24ee05c2d76b8459e8a47f912d71914890533ee
Ganesh <205735160+luxHospitlas@users.noreply.github.com>
2025-05-13 17:16:57 +0530

    test page added

---
+++ app/checking/page.jsx
@@ -0,0 +1,23 @@
+"use client";
+import { handleWhatsappConnection } from "../../app/careconsole";
+export default function CheckingPage() {
+  return (
+    <>
+      <div className="mt-6 flex items-center justify-center md:justify-start space-x-2 bg-white rounded-full px-4 py-2 w-fit">
+        <input
+          type="text"
+          id="messageInput"
+          placeholder="Ask Me Anything"
+          className="bg-transparent text-gray-700 focus:outline-none"
+        />
+        <button
+          type="button"
+          onClick={() => handleWhatsappConnection("LuxGPT")}
+          className="pointer-events-auto bg-[#964F9C] text-white p-2 rounded-full w-10 h-10"
+        >
+          ➜
+        </button>
+      </div>
+    </>
+  );
+}
