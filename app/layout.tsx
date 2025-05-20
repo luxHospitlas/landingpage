d5f5cfc5ce11790b75a68b440561744ed77cfbbc
Ganesh <205735160+luxHospitlas@users.noreply.github.com>
2025-05-20 18:40:33 +0530

    testing gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,18 +47,22 @@ export default function RootLayout({
           type="font/woff2"
           crossOrigin="anonymous"
         />
-        <Script id="gtm-head" strategy="afterInteractive">
+        <Script id="gtm-head" strategy="beforeInteractive">
           {`
-        
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5BCR427Z');
-
-
-          `}
+    console.log("GTM script running...");
+    (function(w,d,s,l,i){
+      console.log("Injecting GTM...");
+      w[l]=w[l]||[];
+      w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+      var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+      j.async=true;
+      j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+      f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5BCR427Z');
+  `}
         </Script>
+
         <ClientHead />
       </head>
       <body>
@@ -68,8 +72,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             height="0"
             width="0"
             style={{
-              display: 'none',
-              visibility:"hidden"            
+              display: "none",
+              visibility: "hidden",
             }}
           ></iframe>
         </noscript>
