d3bca345d797e283ed20cd9913863db82dca02e4
Ganesh <205735160+luxHospitlas@users.noreply.github.com>
2025-05-20 19:01:32 +0530

    dangerous html

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -8,29 +8,6 @@ import Script from "next/script";
 const ClientLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <Script id="gtm-head" strategy="afterInteractive">
-        {`
-        
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5BCR427Z');
-
-
-          `}
-      </Script>
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-5BCR427Z"
-          height="0"
-          width="0"
-          style={{
-            display: "none",
-            visibility: "hidden",
-          }}
-        ></iframe>
-      </noscript>
       {children}
       {/* <MobileStickyFooter mobileNumber="tel:07969084446"/> */}
     </>
